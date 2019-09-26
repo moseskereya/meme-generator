67941f47e5c8929aab8449e764166bcf1162a70c
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-26 16:18:19 +0300

    Updates

---
+++ precache-manifest.a1d174b0ca13b4584610c0b6eb103b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017653c91f32a16ae1fd0e4c95ca0f3d",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "814c56d16b0fda7d09d6",
+    "url": "/meme-generator/static/css/main.7108c504.chunk.css"
+  },
+  {
+    "revision": "57f7e7a92d9c1031a9fb",
+    "url": "/meme-generator/static/js/2.01cbdb52.chunk.js"
+  },
+  {
+    "revision": "814c56d16b0fda7d09d6",
+    "url": "/meme-generator/static/js/main.9c836b86.chunk.js"
+  },
+  {
+    "revision": "b6bd2e30e3283e600112",
+    "url": "/meme-generator/static/js/runtime-main.949914e6.js"
+  }
+]);
